89b12a71b961b8049dbf940c0a098cfcb847681c
Kurt Hammes <kilowatt11@cableone.net>
2016-08-08 21:42:07 -0600

    adding firebase

--- server-assets/models/data-adapter.js
+++ server-assets/models/data-adapter.js
@@ -4,6 +4,10 @@
   JsData = require('js-data'),
   Schemator = require('js-data-schema'),
   NeDbAdapter = require('js-data-nedb'),
+  FirebaseAdapter = require('js-data-firebase')
+  fbAdapter = new FirebaseAdapter({
+    basePath:process.env.DBCONNECTION || 'https://starsandmoons-78919.firebaseio.com/'
+  })
   schemator = new Schemator(),
   adapter = new NeDbAdapter(),
   DS = new JsData.DS();
